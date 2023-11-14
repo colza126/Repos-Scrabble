eeefdb35300ee9979695fc8685a0ade2a9d87ebe
finazzi_andrea <three.point.fina@gmail.com>
2023-11-14 12:52:07 +0100

    inizio della realizzazione dei controlli sull'integrità dell'input del giocatore (contorllo come vengono inserite le caselle nella tabella)

--- server/Tabella.java
+++ server/Tabella.java
@@ -4,42 +4,79 @@ class Tabella {
     // numero delle caselle per ogni riga e per ogni colonna
     public final static int NUM_CASELLE = 15;
     // matrice di caselle
-    Casella[][] tabella = new Casella[NUM_CASELLE][NUM_CASELLE];
+    Casella[][] tabella;
 
     // metodi
+    // costruttore di default
+    public Tabella() {
+        this.tabella = new Casella[NUM_CASELLE][NUM_CASELLE];
+    }
+
     // costruttore con parametri
     public Tabella(Casella[][] _tabella) {
         this.tabella = _tabella;
     }
 
-    public boolean controlloLibera(int x,int y,char lettera){
+    /*public boolean controlloLibera(int x,int y,char lettera){
         if(tabella[x][y] == null || tabella[x][y].lettera == lettera){
             return true;
         }
         return false;
-    }
-/* 
-    public boolean controlloCentrale(int x,int y,char lettera){
-        if(tabella[x][y] == null || tabella[x][y].lettera == lettera){
-            return true;
-        }
-        return false;
-    }
-*/
-    public boolean controlloMaster(int x, int y,String parola,char direzione){
-        for (int i = 0; i < parola.length(); i++) {
-            if(direzione == 'r'){
-                if(!controlloLibera(x+i,y,parola.charAt(i))){
-                    return false;
-                }
-            }else if(direzione == 'd'){
-                if(!controlloLibera(x,y+i,parola.charAt(i))){
-                    return false;
-                }
-            }
+    }*/
+
+    //metodo nel quale si richiamano tutti i controlli
+    public boolean controlloMaster(Richiesta _richiesta){
+        //variabile d'appoggio
+        boolean work=true;
+        char direction= ' ';
+
+        //1. controllo direzione di inserimento della parola
+        //verifica direzione
+        direction = _richiesta.checkDirezione();
+        //controllo integrità
+        if(direction == ' ')
+            return false;
+
+        //2. controllo che la casella centrale non sia vuota
+        //controllo se ci si trova nel primo inserimento --> in questo caso la casella centrale deve obbligatoriamente essere occupata
+        work = controllaCasellaCentrale(_richiesta, direction);
             
-        }
+        
         return true;
+    }
+
+    //controllo che la casella centrale della tabella sia piena
+    public boolean controllaCasellaCentrale(Richiesta _richiesta, char _direzione){
+        //variabile di lavoro
+        boolean work=true;
+
+        //controllo se ci si trova al primo inserimento
+        //scorrimento righe
+        for(int i=0; i < NUM_CASELLE; i++){
+            //scorrimento colonne
+            for(int j=0; j < NUM_CASELLE;j++){
+                //controllo se le caselle sono tutte vuote
+                if(tabella[i][j].lettera != ' ')
+                    //casella piena
+                    work=false;
+            }
+        }
+        
+        //se le caselle non sono tutte vuote è già avvenuto il primo inserimento e lì è stato effettuato questo controllo, se l'esecuzione 
+        //è proseguita significa che la centrale è occupata
+        if(!work)
+            return true;
+        //se ci si trova nel primo inserimento bisogna controllare che si voglia inserire qualcosa nella casella centrale
+        else{
+            //se la parola deve essere insirita in verticale
+            if(_direzione == 'l')
+            {
+                //controllo che la colonna di inserimento sia quella centrale (altrimenti è impossibile passare dal centro)
+                if(_richiesta.xInizio != 7)
+                    //casella centrale non inserita
+                    return false;
+            }
+        }
     }
 
     public void aggiungiParola(int x, int y,String parola,char direzione){
@@ -58,7 +95,7 @@ class Tabella {
     // metodo che assegna il valore ad ogni casella della tabella (valore relativo al moltiplicatore di ogni singola casella)
     public void assegnaValori() {
         //variabile d'appoggio
-        int moltiplicatore = 1;
+        //int moltiplicatore = 1;
 
         //scorrimento righe
         /*for (int row = 0; row <= 14; row++) {
--- server/server.java
+++ server/server.java
@@ -1,16 +1,13 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 //gestione del server
-
-import java.io.IOException;
-import java.net.*;
-
 class server{
     //elementi per la comunicazione con il client
     private static final int SOCKET_PORT = 666;
-    private static final int BYTE_BUFFER = 1500;
+    //private static final int BYTE_BUFFER = 1500;
 
     //dichiarazione oggetti globali
     static DatagramSocket socket;
@@ -22,7 +19,11 @@ class server{
         //booleano che identifica lo status della comunicazione
         boolean running = true;
         //richiesta del client
-        String richiesta="";
+        String richiesta;
+        //direzione della parola (in base a come la inserisce l'utente)
+        char direzione;
+        //dichiarazione tabella
+        Tabella tab = new Tabella();
 
         //server in attesa di una comunicazione da parte del client
         socket = new DatagramSocket(SOCKET_PORT);
@@ -33,16 +34,16 @@ class server{
             //lettura della richiesta del client e salvataggio in apposita variabile
             richiesta = attendiRichiesta(socket);
 
-            //-----------a questo punto richiesta contiene una stringa nel seguente formato "parola;xInizio;yInizio;xFine;yFine"----------//
+            //----------a questo punto richiesta contiene una stringa nel seguente formato "parola;xInizio;yInizio;xFine;yFine"----------\\
 
             //creazione di una richiesta (la stringa precedente viene convertita in un oggetto con 5 attributi)
             Richiesta richiestaClient = new Richiesta();
             //salvataggio degli attributi passati dal client nella richiesta
             richiestaClient.creaRichiesta(richiesta);
 
-            //-----------a questo punto richiesta contiene l'input del client suddiviso in un insieme di attributi----------//
-
-            
+            //----------a questo punto richiesta contiene l'input del client suddiviso in un insieme di attributi----------\\
+            //metodo che controlla l'integrità dell'input del client (direzione della parola, inserimento nelle caselle consecutive, lettere che non escono dalla tabella ecc.)
+            boolean statoInserimento = tab.controlloMaster(richiestaClient);
         }
 
         //chiudo la socket
@@ -71,5 +72,15 @@ class server{
         
         //restituzione della stringa ottenuta
         return buff;
+    }
+
+    /**
+     * metodo per inviare la risposta al client
+     * @param risposta String contenente la risposta da inviare al client
+     * @throws IOException
+     */
+    public static void inviaRisposta(String risposta) throws IOException
+    {
+        /////implementare metodo che invii una stringa di risposta al client
     }
 }
